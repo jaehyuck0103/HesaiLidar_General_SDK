d8995a7855d0d3a8845f999342bb8ace2830d7a8
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2022-03-01 23:50:34 +0900

    pointcloud conversion by Eigen.

--- test/runSDK.cpp
+++ test/runSDK.cpp
@@ -2,9 +2,16 @@
 #include "pandar/pandar_lidar_receiver.h"
 #include "pandar/pandar_utils.h"
 
+#include <Eigen/Dense>
 #include <pcl/visualization/cloud_viewer.h>
 #include <tbb/concurrent_queue.h>
 #include <thread>
+
+#include <chrono>
+
+using namespace std::chrono_literals;
+using std::chrono::duration;
+using std::chrono::system_clock;
 
 pcl::visualization::CloudViewer viewer("Simple");
 
@@ -12,42 +19,76 @@ tbb::concurrent_bounded_queue<std::vector<uint8_t>> con_queue;
 
 double degToRad(double degree) { return degree * M_PI / 180; }
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr
-calcPointXYZI(const std::vector<uint8_t> &frame_buffer, const PandarConfig &cfg) {
+class PCLConverter {
+  private:
+    Eigen::ArrayXf elev_angle_rad_sin_;
+    Eigen::ArrayXf elev_angle_rad_cos_;
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = pcl::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+    Eigen::ArrayXf azimuth_map_rad_sin_;
+    Eigen::ArrayXf azimuth_map_rad_cos_;
 
-    for (int azimuth_idx = 0; azimuth_idx < cfg.frame_buffer_width(); ++azimuth_idx) {
-        for (int return_idx = 0; return_idx < cfg.num_returns(); ++return_idx) {
-            for (int laser_idx = 0; laser_idx < cfg.num_lasers(); ++laser_idx) {
+    const PandarConfig cfg_;
 
-                const int bytes_index =
-                    cfg.get_frame_buffer_bytes_index(azimuth_idx, return_idx, laser_idx);
+  public:
+    PCLConverter(const PandarConfig &cfg) : cfg_(cfg) {
 
-                uint16_t raw_distance = frame_buffer[bytes_index] | frame_buffer[bytes_index + 1]
-                                                                        << 8;
-                // uint8_t intensity = frame_buffer[bytes_index + 2];
+        const auto elev_angle_rad =
+            Eigen::ArrayXf::Map(cfg.elev_angle().data(), cfg.elev_angle().size()) * M_PI / 180.f;
 
-                float distance = raw_distance * cfg.unit_distance;
-                if (distance < 0.1 || distance > 200.0) {
-                    continue;
-                }
+        elev_angle_rad_cos_ =
+            elev_angle_rad.cos().replicate(cfg.num_returns() * cfg.frame_buffer_width(), 1);
+        elev_angle_rad_sin_ =
+            elev_angle_rad.sin().replicate(cfg.num_returns() * cfg.frame_buffer_width(), 1);
 
-                const uint16_t azimuth = cfg.azimuthIdxToAzimuth(azimuth_idx);
+        const auto azimuth_offset_rad =
+            Eigen::ArrayXf::Map(cfg.azimuth_offset().data(), cfg.azimuth_offset().size()) * M_PI /
+            180.f;
 
-                float xyDistance = distance * cosf(degToRad(cfg.elev_angle()[laser_idx]));
-                float x = xyDistance *
-                          sinf(degToRad(cfg.azimuth_offset()[laser_idx] + azimuth / 100.0f));
-                float y = xyDistance *
-                          cosf(degToRad(cfg.azimuth_offset()[laser_idx] + azimuth / 100.0f));
-                float z = distance * sinf(degToRad(cfg.elev_angle()[laser_idx]));
+        Eigen::ArrayXXf azimuth_rad = Eigen::ArrayXXf::Zero(1, cfg.frame_buffer_width());
+        for (int i = 0; i < cfg.frame_buffer_width(); ++i) {
+            azimuth_rad(0, i) = degToRad(cfg.azimuthIdxToAzimuth(i) / 100.0f);
+        }
 
-                cloud->emplace_back(x, y, z);
+        const auto azimuth_map_rad =
+            azimuth_rad.replicate(cfg.num_lasers(), 1).colwise() + azimuth_offset_rad;
+        azimuth_map_rad_sin_ = azimuth_map_rad.sin().replicate(cfg.num_returns(), 1).reshaped();
+        azimuth_map_rad_cos_ = azimuth_map_rad.cos().replicate(cfg.num_returns(), 1).reshaped();
+    }
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr calcPointXYZ(const std::vector<uint8_t> &frame_buffer) {
+
+        const auto raw_distance_lower =
+            Eigen::Map<const Eigen::ArrayX<uint8_t>, Eigen::Unaligned, Eigen::InnerStride<3>>{
+                frame_buffer.data(),
+                cfg_.frame_buffer_elems()};
+
+        const auto raw_distance_upper =
+            Eigen::Map<const Eigen::ArrayX<uint8_t>, Eigen::Unaligned, Eigen::InnerStride<3>>{
+                frame_buffer.data() + 1,
+                cfg_.frame_buffer_elems()};
+
+        const auto distance =
+            (raw_distance_lower.cast<float>() + raw_distance_upper.cast<float>() * 256) *
+            cfg_.unit_distance;
+
+        const auto xyDistance = distance * elev_angle_rad_cos_;
+
+        const auto x = xyDistance * azimuth_map_rad_sin_;
+        const auto y = xyDistance * azimuth_map_rad_cos_;
+        const auto z = distance * elev_angle_rad_sin_;
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud =
+            pcl::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+        const auto is_valid_distance = ((distance > 0.1) * (distance < 200.0)).cast<int>();
+        cloud->reserve(is_valid_distance.sum());
+        for (int i = 0; i < distance.size(); ++i) {
+            if (is_valid_distance[i] > 0) {
+                cloud->emplace_back(x[i], y[i], z[i]);
             }
         }
+        return cloud;
     }
-    return cloud;
-}
+};
 
 void gpsCallback(int timestamp) { std::cout << "timestamp: " << timestamp << "\n"; }
 
@@ -87,9 +128,13 @@ int main() {
     pandarGeneral.start();
 
     std::vector<uint8_t> cld;
+    PCLConverter converter{pandar_config};
     while (true) {
         con_queue.pop(cld);
-        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = calcPointXYZI(cld, pandar_config);
+        system_clock::time_point start = system_clock::now();
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = converter.calcPointXYZ(cld);
+        const duration<double, std::milli> due = system_clock::now() - start;
+        std::cout << due.count() << "\n";
         viewer.showCloud(cloud);
     }
 
